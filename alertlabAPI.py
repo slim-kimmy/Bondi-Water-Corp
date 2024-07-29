a0f2de297594b99aebb8cacdffb1c729e50a28e4
Ethan Kim <83152150+slim-kimmy@users.noreply.github.com>
2024-07-29 12:25:30 -0400

    Add files via upload

---
+++ alertlabAPI.py
@@ -0,0 +1,199 @@
+import requests
+import json
+
+import pandas as pd
+from datetime import datetime, timedelta
+import urllib.parse as urlparse
+
+
+def read_credentials(file_path="credentials.txt"):
+    username = None
+    password = None    
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            if line.startswith('username:'):
+                username = line.split('username:')[1].strip()
+            elif line.startswith('password:'):
+                password = line.split('password:')[1].strip()
+    return username, password
+
+def get_token(username, password):
+    url = 'https://www.alertlabsdashboard.com/api/v3/login'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'user': username,
+        'password': password
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+    if response.status_code == 202: 
+        print("Request was accepted.")
+        response_json = response.json()
+        print(response_json)
+        # Extract the token from the response
+        token = response_json.get("token")
+        print("Access Token:", token)
+    else:
+        print(f"Failed to retrieve token. Status code: {response.status_code}")
+    return token
+
+
+def check_credential(file_path="token.txt"):
+    # Read the content of the file
+    with open(file_path, 'r') as file:
+        content = file.read().strip()
+    
+    # Extract the token and date from the content
+    token_part, date_part = content.split('date: ')
+    token = token_part.split('token: ')[1].strip()
+    date_str = date_part.strip()
+    file_date = datetime.strptime(date_str, "%m/%d/%Y")
+
+    
+    # Calculate the difference between today's date and the file date
+    today = datetime.now()
+    date_difference = today - file_date
+    
+    # If the date is more than 29 days ago, get a new token and update the file
+    if date_difference.days > 29:
+        username, password = read_credentials()
+        new_token = get_token(username, password)  # Assuming get_token() is defined elsewhere
+        new_date_str = today.strftime("%m/%d/%Y")
+        new_content = f"token: {new_token}\ndate: {new_date_str}"
+        token = new_token
+        # Write the new content to the file
+        with open(file_path, 'w') as file:
+            file.write(new_content)
+    return token
+
+
+
+
+def locations(token='$2b$08$7kPxAMomf57uw1hQWXY7je3h5pAGSilfGe.MRj4o7drjgY9Vj6BJ2'):
+        # Get all locations test
+    test_url = 'https://www.alertlabsdashboard.com/api/v3/dataModel/read/allLocations'
+    test_headers = {
+    'token': token
+    }
+    response = requests.get(test_url, headers=test_headers)
+    response_json = response.json()
+    return response_json["dataModel"]
+
+def get_ongoing(location_id, token='$2b$08$7kPxAMomf57uw1hQWXY7je3h5pAGSilfGe.MRj4o7drjgY9Vj6BJ2'):
+            # Get all locations test
+    test_url = f'https://www.alertlabsdashboard.com/api/v3/dataModel/read/allSensorEventsAtLocation?locationID={location_id}'
+    test_headers = {
+    'token': token
+    }
+    response = requests.get(test_url, headers=test_headers)
+    response_json = response.json()
+    return response_json["dataModel"]
+
+
+#Test 313747173737333821002b00
+def get_timeseries(sensor_id="323747143531313928002000", start_date="1720119038", end_date="1720205438", rate="h", series="water"):
+    test_url = f'https://www.alertlabsdashboard.com/api/v3/timeSeries/sensor/{sensor_id}?start={start_date}&end={end_date}&rate={rate}&series={series}'
+    test_headers = {
+    'token': '$2b$08$7kPxAMomf57uw1hQWXY7je3h5pAGSilfGe.MRj4o7drjgY9Vj6BJ2'
+    }
+    response = requests.get(test_url, headers=test_headers)
+    response_json = response.json()
+    print("GETTING DATA")
+    print(response_json)
+    print(type(response_json))
+    values = response_json['value']
+    # Create a DataFrame
+    df = pd.DataFrame(values, columns=['time', 'series'])
+    df['Datetime'] = pd.to_datetime(df['time'], unit='s') - timedelta(hours=4)
+    return df
+
+def get_list_timeseries(sensor_list, start_date="1720119038", end_date="1720205438", rate="h", series="water"):
+    time_series_list = []
+    for sensor in sensor_list:
+        time_series_data = get_timeseries(sensor_id=sensor,
+                                             start_date=start_date,
+                                             end_date=end_date,
+                                             rate=rate
+                                            )
+        time_series_list.append(time_series_data)
+    return time_series_list
+
+def get_properties_with_sensors(properties, token):
+    for place in properties:
+        location_id = place["_id"]
+        sensors = get_ongoing(location_id, token)
+        list_of_sensors = sensors[0]['sensors']
+        # Use list comprehension to filter sensors
+        sensors_at_location = [sensor["_id"] for sensor in list_of_sensors if sensor['type'] in {"FlowieO", "WaterMeter"}]
+        place["sensors"] = sensors_at_location
+    return properties
+
+LOGIN_API = 'https://www.alertlabsdashboard.com/oauth/login?client_id=AlertLabsLogin&state=login&redirect_uri=https://www.alertlabsdashboard.com&response_type=code'
+TOKEN_API = 'https://www.alertlabsdashboard.com/oauth/AlertLabsLogin/tokenExchange'
+COMPARISON_API = 'https://www.alertlabsdashboard.com/api/v2/aggregates/comparison'
+
+# Handle token exchange token to get new access token
+def generate_new_authorization_header():
+    response = requests.post(LOGIN_API, json={
+        "user": "ethank007@hotmail.com",
+        "password": "Kimmy2002"
+    })
+    login_response = json.loads(response.text)
+    if login_response['success'] == False:
+        return
+    parsed = urlparse.urlparse(login_response['redirectURI'])
+    response = requests.post(TOKEN_API, data={
+            "code": urlparse.parse_qs(parsed.query)['code'],
+            "client_secret": "Siwe98EMfnL973Nner",
+            "grant_type": "authorization_code",
+            "client_id": "AlertLabsLogin"
+        }, headers={
+            "Content-Type": "application/x-www-form-urlencoded"
+        })
+    secret = json.loads(response.text)
+    return secret['access_token']
+
+# Return false if error, return json if successful
+def getComparisonData(locationIDs):
+    locationIDs = json.dumps({
+        "locationIDs": locationIDs
+    })
+    response = requests.get(COMPARISON_API, params=locationIDs, headers={
+        "authorization": generateNewAuthorizationHeader()
+    })
+    if response.status_code != 200:
+        return False
+    return json.loads(response.text)
+
+def sum_columns(dataframes, column_names):
+    if len(dataframes) > 1:
+        # Initialize a dataframe with the structure of the first dataframe in the list
+        summed_df = dataframes[0].copy()
+        # Iterate through the list of dataframes, starting from the second dataframe
+        for df in dataframes[1:]:
+            for col in column_names:
+                # Sum the specified columns
+                summed_df[col] += df[col]
+        return summed_df
+    elif len(dataframes) == 1:
+        return dataframes[0]
+    
+
+def main():
+    token = check_credential()
+    properties = locations(token)
+    properties_with_sensors = get_properties_with_sensors(properties, token)
+    df_locations = pd.DataFrame.from_dict(properties_with_sensors)
+    return df_locations
+
+
+
+"""
+To do list:
+1. Create a function that will update the list of sensors
+2. Create a dataframe that has a row each for every location ID and a then a column for a list of sensors
+3. Create function to query for the average volume by date
+"""
---
+++ dashboard-test.py
@@ -0,0 +1,167 @@
+import ast
+import time
+import pytz
+import pandas as pd
+from datetime import datetime, timedelta
+import streamlit as st
+import altair as alt
+import plotly.express as px
+import alertlabAPI as al
+import streamlit_toggle as tog
+
+
+###########################################
+def get_60_day_monday_average(sensor_list):
+    if len(sensor_list) > 0:
+        # Get today's date and convert to unix time
+        today = datetime.now()
+        today_unix = int(time.mktime(today.timetuple()))
+        # Get 60 days ago date and convert to unix time
+        sixty_days_ago = today - timedelta(days=60)
+        sixty_days_ago_unix = int(time.mktime(sixty_days_ago.timetuple()))
+        # Query for all the sensors at the location
+        sixty_day_dataframes = al.get_list_timeseries(sensor_list, start_date=sixty_days_ago_unix, end_date=today_unix, rate="h", series="water")
+        # Sum the dataframes
+        cumulative_sixty_day_consumption = al.sum_columns(sixty_day_dataframes, ['series'])
+        # Convert the datetime strings into Datetime objects and adjust for UTC to EDT
+        cumulative_sixty_day_consumption['Datetime'] = pd.to_datetime(cumulative_sixty_day_consumption['Datetime'])
+        # Filter the data for Datetime values that are Mondays at 3AM EDT
+        filtered_data = cumulative_sixty_day_consumption[(cumulative_sixty_day_consumption['Datetime'].dt.dayofweek == 0) & (cumulative_sixty_day_consumption['Datetime'].dt.hour == 3)]
+        # Calculate the mean and median of the series column for the filtered data
+        mean_series = filtered_data['series'].mean()
+        median_series = filtered_data['series'].median()
+        return mean_series, median_series, cumulative_sixty_day_consumption
+    
+def get_this_weeks_average():
+    pass
+
+def make_timeseries_chart(queried_sensors, start_date, end_date, rate, series):
+    if len(queried_sensors) != 0:
+        time_series_data = al.get_list_timeseries(queried_sensors, start_date=start_date_unix, end_date=end_date_unix, rate=rate, series=series)
+        # Sum the displayed dataframes
+        cumulative_timeseries_data = al.sum_columns(time_series_data, ['series'])
+        # Generate the chart
+        st.bar_chart(cumulative_timeseries_data, x="Datetime", y="series", x_label="Date", y_label="Water Consumption", height=800)
+        st.write(cumulative_timeseries_data)
+
+def get_location_dataframes(location_id):
+    pass
+###########################################
+
+    
+# Settings
+st.set_page_config(
+    page_title="Bondi Water Corp",
+    page_icon=":potable_water:",
+    layout="wide",
+    initial_sidebar_state="expanded")
+alt.themes.enable("dark")
+# Read in existing CSV, these will be swapped every refresh
+if 'df' not in st.session_state:
+    initial_load_dataframe = al.main()
+    # Creating unique ID's from a concatenation of address and name values
+    initial_load_dataframe["unique"] = initial_load_dataframe["address"] + " " + initial_load_dataframe["name"]
+    # Stashing in session
+    st.session_state.df = initial_load_dataframe
+df = st.session_state.df
+
+
+
+with st.sidebar:
+    # Dashboard title
+    st.markdown("""
+    <style>
+    .big-font {
+        font-size:40px !important;
+        color: #122B46; 
+    }
+    .kpi-font {
+        font-size:35px !important;
+        color: #ed0919; 
+    </style>
+    """, unsafe_allow_html=True)
+    st.markdown('<p class="big-font">Water Consumption Dashboard</p>', unsafe_allow_html=True)
+
+    # Address Filter Dropdown
+    address_list = list(df.unique.unique())[::-1]
+    selected_address = st.selectbox('Select Property:', address_list)
+    df_selected_address = df[df.unique == selected_address]
+    st.write(df_selected_address)
+    # Calendar widget 
+    start_date = st.date_input("Start Date")
+    end_date = st.date_input("End Date")
+    start_date_unix = str(datetime.strptime(str(start_date), "%Y-%m-%d").timestamp())
+    end_date_unix = str(datetime.strptime(str(end_date), "%Y-%m-%d").timestamp())
+
+    # Rate selection
+    rate_switch = tog.st_toggle_switch(label="Hour / Minute")
+    rate = "h"
+    if rate_switch == True:
+        rate="m"
+    # Series selection
+    series_switch = tog.st_toggle_switch(label="Water / Temperature")
+    series = "water"
+    if series_switch == True:
+        series="temp"
+    # List of sensors as buttons
+    buttons = []
+    sensor_list = df_selected_address["sensors"].iloc[0]
+    for i in sensor_list:
+        buttons.append(tog.st_toggle_switch(label=i,
+                                            key=i))
+    for button in buttons:
+        if button:
+            pass
+    # Time series data list
+    queried_sensors = []
+    for sensor_id, button in zip(sensor_list, buttons):
+        if button == True:
+            queried_sensors.append(sensor_id)        
+    # Initiate Query and get list of dataframes from selected sensors
+    submitted = st.button("Query")
+
+
+# Displays
+    # KPI's
+mean, median, df2 = get_60_day_monday_average(sensor_list)
+
+
+st.markdown(
+    """
+<style>
+[data-testid="stMetricValue"] {
+    font-size: 100px;
+}
+div[data-testid="stMarkdownContainer"] > p {
+    font-size: 35px;
+}
+
+
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+
+kpi1, kpi2, kpi3 = st.columns(3)
+kpi1.metric(
+    label="Average Litres",
+    value=round(mean)
+)
+kpi2.metric(
+    label="Median Litres",
+    value=round(median)
+)
+kpi3.metric(
+    label="Past Weeks Avg",
+    value = 999)
+
+
+    # Timeseries chart
+if submitted == True:
+    # Function to make timeseries chart  
+    make_timeseries_chart(queried_sensors, start_date_unix, end_date_unix, rate, series)
+
+
+
+
